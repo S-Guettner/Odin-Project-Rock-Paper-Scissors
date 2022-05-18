2d8a1c32a88f3ff63080c83e65549650c3e82700
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-05-19 00:45:51 +0200

    function to choose random item from itemsArray

---
+++ script.js
@@ -0,0 +1,13 @@
+itemsArray = ["Stone","Paper","Scissor"];
+ let item = "";
+
+function computerPlay(){
+   
+    return itemsArray[Math.floor(Math.random()*itemsArray.length)];
+    
+    
+}
+
+
+randomItem = computerPlay(itemsArray);
+
