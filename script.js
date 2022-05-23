c426642838d42a96c2862b1843e49acd40c652bf
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-05-23 22:13:11 +0200

    added playRound function

--- script.js
+++ script.js
@@ -3,11 +3,39 @@ itemsArray = ["Stone","Paper","Scissor"];
 
 function computerPlay(){
    
+
     return itemsArray[Math.floor(Math.random()*itemsArray.length)];
+}
+
+function playRound (playerSelection,computerSelection){
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock"
+    }
     
+    if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock"
+    }
+
+    if (playerSelection == "Scissor" && computerSelection == "Paper") {
+        return "You Win! Scissor beats Paper"
+    }
     
+    if (playerSelection == "Paper" && computerSelection == "Scissor") {
+        return "You Lose! Scissor beats Paper"
+    }
+
+    if (playerSelection == "Rock" && computerSelection == "Scissor") {
+        return "You Win! Rock beats Scissor"
+    }
+
+    if (playerSelection == "Scissor" && computerSelection == "Rock") {
+        return "You Lose! Rock beats Scissor"
+    }
 }
 
 
-randomItem = computerPlay(itemsArray);
 
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection))
