a51e6865fe2a8f507b44941c04b64c0a631ca0b3
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-05-25 19:16:02 +0200

    Player input can be lower or uppercase

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 itemsArray = ["Stone","Paper","Scissor"];
  let item = "";
 
+ /* Function returns random item from itemsArray */
 function computerPlay(){
-   
-
-    return itemsArray[Math.floor(Math.random()*itemsArray.length)];
+   return itemsArray[Math.floor(Math.random()*itemsArray.length)];
 }
 
+
+
+/* Function that checks for winner and returns message*/ 
+
+
+const playerSelection = prompt ("What is your Weapon of Choice?");
+const computerSelection = computerPlay();
+const weaponSelection = playerSelection.toLowerCase();
+
 function playRound (playerSelection,computerSelection){
-    if (playerSelection == "Rock" && computerSelection == "Paper") {
+
+    
+
+    if (weaponSelection == "rock" && computerSelection == "Paper") {
         return "You Lose! Paper beats Rock"
     }
-    
-    if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You Win! Paper beats Rock"
-    }
 
-    if (playerSelection == "Scissor" && computerSelection == "Paper") {
-        return "You Win! Scissor beats Paper"
-    }
-    
-    if (playerSelection == "Paper" && computerSelection == "Scissor") {
-        return "You Lose! Scissor beats Paper"
-    }
-
-    if (playerSelection == "Rock" && computerSelection == "Scissor") {
+    if (weaponSelection == "rock" && computerSelection == "Scissor") {
         return "You Win! Rock beats Scissor"
     }
 
-    if (playerSelection == "Scissor" && computerSelection == "Rock") {
+    if (weaponSelection == "paper" && computerSelection == "Scissor") {
+        return "You Lose! Scissor beats Paper"
+    }
+
+    if (weaponSelection == "paper" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock"
+    }
+
+    if (weaponSelection == "scissor" && computerSelection == "Paper") {
+        return "You Win! Scissor beats Paper"
+    }
+    
+    
+    if (weaponSelection == "scissor" && computerSelection == "Rock") {
         return "You Lose! Rock beats Scissor"
     }
 }
+console.log(playRound(playerSelection, computerSelection));
 
 
-
-const playerSelection = "Rock";
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection))
