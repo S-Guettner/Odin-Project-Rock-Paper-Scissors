b182a20b3b3717330a814c578ac34fa34161cbc9
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-07-13 18:44:43 +0200

    optimized code and added undecided

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-itemsArray = ["Stone","Paper","Scissor"];
- let item = "";
+let choiceArray ["rock,paper,scissor"];
 
- /* Function returns random item from itemsArray */
-function computerPlay(){
-   return itemsArray[Math.floor(Math.random()*itemsArray.length)];
+function computerPlay (choiceArray){
+    return choiceArray[Math.floor(Math.random()*items.length)];
+     
+
 }
-
-
-
-/* Function that checks for winner and returns message*/ 
-
-
-const playerSelection = prompt ("What is your Weapon of Choice?");
-const computerSelection = computerPlay();
-const weaponSelection = playerSelection.toLowerCase();
-
-function playRound (playerSelection,computerSelection){
-
-    
-
-    if (weaponSelection == "rock" && computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock"
-    }
-
-    if (weaponSelection == "rock" && computerSelection == "Scissor") {
-        return "You Win! Rock beats Scissor"
-    }
-
-    if (weaponSelection == "paper" && computerSelection == "Scissor") {
-        return "You Lose! Scissor beats Paper"
-    }
-
-    if (weaponSelection == "paper" && computerSelection == "Rock") {
-        return "You Win! Paper beats Rock"
-    }
-
-    if (weaponSelection == "scissor" && computerSelection == "Paper") {
-        return "You Win! Scissor beats Paper"
-    }
-    
-    
-    if (weaponSelection == "scissor" && computerSelection == "Rock") {
-        return "You Lose! Rock beats Scissor"
-    }
-}
-console.log(playRound(playerSelection, computerSelection));
-
-
+console.log(computerPlay(choiceArray));
