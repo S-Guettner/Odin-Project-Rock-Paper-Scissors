b63963c426e4250bab71eaf74b6192771e743503
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-07-13 18:54:43 +0200

    optimized code and added undecided

--- script.js
+++ script.js
@@ -1,8 +1,83 @@
-let choiceArray ["rock,paper,scissor"];
+itemsArray = ["stone","paper","scissor"];
 
-function computerPlay (choiceArray){
-    return choiceArray[Math.floor(Math.random()*items.length)];
-     
 
+ /* Function returns random item from itemsArray */
+function computerPlay(){
+   return itemsArray[Math.floor(Math.random()*itemsArray.length)];
 }
-console.log(computerPlay(choiceArray));
+
+let playerInput = window.prompt("Your Weapon of choice?");
+let playerSelection = playerInput.toLowerCase();
+let computerSelection = computerPlay();
+
+
+function gamePlay(computerSelection,playerSelection){
+
+    if (computerSelection == "rock" && playerSelection == "paper"){
+    return "You Win! Paper beats Rock.";}
+    
+
+    else if (computerSelection == "rock" && playerSelection == "scissor"){
+    return "You Lose! Rock beats scissor.";}
+
+  
+
+    else if (computerSelection == "rock" && playerSelection == "rock"){
+    return "No Winner!";}
+
+   
+    
+    else if (computerSelection == "paper" && playerSelection == "paper"){
+    return "No Winner!";}
+
+   
+    
+    else if (computerSelection == "paper" && playerSelection == "rock"){
+        return "You Lose! Paper beats Rock.";}
+
+       
+
+    else if (computerSelection == "paper" && playerSelection == "scissor"){
+        return "You Win! Scissor beats Paper.";}    
+
+ 
+
+
+     else if (computerSelection == "scissor" && playerSelection == "rock"){
+    return "You Win! Rock beats scissor.";}
+
+
+    
+    else if (computerSelection == "scissor" && playerSelection == "paper"){
+        return "You Lose! Scissor beats Paper.";}
+
+
+    else if (computerSelection == "scissor" && playerSelection == "scissor"){
+        return "No Winner!.";}        
+    
+        else {
+
+            return "Wrong Input!!!!!!!!!";}
+        
+        
+}
+
+function game(){
+
+
+for (let i = 0; i < 5; i++) {
+    let playerInput = window.prompt("Your Weapon of choice?");
+    let playerSelection = playerInput.toLowerCase();
+    let computerSelection = computerPlay();
+    
+    gamePlay(computerSelection,playerSelection);
+ }
+}
+
+console.log(game());
+
+
+
+
+
+
