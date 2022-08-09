f061fe24f7954508fe7b596ddf33787c492d1a62
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-08-09 15:07:50 +0200

    updated gamePlay function. output now working

--- script.js
+++ script.js
@@ -66,62 +66,8 @@ else if (computerSelection == "scissor" && playerSelection == "scissor"){
 }
 
 /* game mechanic */
-function game(){
-
-    gamePlay(playerInput);
-
-    if (computerSelection == "rock" && playerSelection == "paper"){
-        return "You Win! Paper beats Rock."}
-    
-
-    else if (computerSelection == "rock" && playerSelection == "scissor"){
-        return "You Lose! Rock beats scissor.";}
-
-  
-
-    else if (computerSelection == "rock" && playerSelection == "rock"){
-        return "No Winner!";}
-
-   
-    
-    else if (computerSelection == "paper" && playerSelection == "paper"){
-        return "No Winner!";}
-
-   
-    
-    else if (computerSelection == "paper" && playerSelection == "rock"){
-        return "You Lose! Paper beats Rock.";}
-
-       
-
-    else if (computerSelection == "paper" && playerSelection == "scissor"){
-        return "You Win! Scissor beats Paper.";}    
-
- 
-
-
-     else if (computerSelection == "scissor" && playerSelection == "rock"){
-        return "You Win! Rock beats scissor.";}
-
-
-    
-    else if (computerSelection == "scissor" && playerSelection == "paper"){
-        return "You Lose! Scissor beats Paper.";}
-
-
-    
-    else if (computerSelection == "scissor" && playerSelection == "scissor"){
-        return "No Winner!.";}        
-    
-    else {
-
-        return "Wrong Input!!!!!!!!!";}
-        
-      /* game mechanic */
-}
 
 
 
 
 
-/* Input working, but function not returning result */
