4e7180bb5be102ceda8f153809d53fff90450903
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-08-09 15:07:15 +0200

    updated gamePlay function. output now working

--- script.js
+++ script.js
@@ -17,10 +17,58 @@ function gamePlay(playerInput){
 /*Input from Button */
 let playerSelection = playerInput;
 
-console.log(playerInput)
+if (computerSelection == "rock" && playerSelection == "paper"){
+    console.log("You Win Paper beats Rock")  }
 
+
+else if (computerSelection == "rock" && playerSelection == "scissor"){
+    console.log("You Lose! Rock beats scissor.") }
+
+
+
+else if (computerSelection == "rock" && playerSelection == "rock"){
+    console.log("No Winner!")  }
+
+
+
+else if (computerSelection == "paper" && playerSelection == "paper"){
+    console.log("No Winner!")  }
+
+
+
+else if (computerSelection == "paper" && playerSelection == "rock"){
+    console.log("You Lose! Paper beats Rock.")  }
+
+   
+
+else if (computerSelection == "paper" && playerSelection == "scissor"){
+    console.log("You Win! Scissor beats Paper.") }    
+
+
+
+
+ else if (computerSelection == "scissor" && playerSelection == "rock"){
+    console.log("You Win! Rock beats scissor.")}
+
+
+
+else if (computerSelection == "scissor" && playerSelection == "paper"){
+    console.log("You Lose! Scissor beats Paper.") }
+
+
+
+else if (computerSelection == "scissor" && playerSelection == "scissor"){
+    console.log("No Winner!.") }     
+
+
+
+
+}
 
 /* game mechanic */
+function game(){
+
+    gamePlay(playerInput);
 
     if (computerSelection == "rock" && playerSelection == "paper"){
         return "You Win! Paper beats Rock."}
@@ -77,5 +125,3 @@ console.log(playerInput)
 
 
 /* Input working, but function not returning result */
-
-
